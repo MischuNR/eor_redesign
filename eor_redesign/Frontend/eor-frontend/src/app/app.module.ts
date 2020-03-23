cf9939eeca9b53daae8432566ba4e0b4ebb8a7c6
Michel Rohrbach <michelrohrbach@Michels-MacBook-Pro.local>
2020-03-23 11:09:50 +0100

    Navbar and Footer are almost done. Login is under construction

--- eor_redesign/Frontend/eor-frontend/src/app/app.module.ts
+++ eor_redesign/Frontend/eor-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContentComponent } from './components/content/content.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
